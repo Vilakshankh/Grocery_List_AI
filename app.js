dd586990f88b4eb7aaf840b926f9dd6c734dc04f
VilakshanKh <31506996+Vilakshankh@users.noreply.github.com>
2024-08-29 12:33:12 -0400

    basic error handling

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 document.getElementById('groceryForm').addEventListener('submit', function(event) {
-
     event.preventDefault(); // Prevent the form from submitting and the page refreshing
-    const userInput = document.getElementById('userRequest').value;
-    console.log('You entered: ' + userInput);
+
+
+    try {
+        const userInput = document.getElementById('userRequest').value;
+        console.log('generating meal plan for: ' + userInput);
+
+        setTimeout(() => {
+            const mealPlan = "Meal Plan for " + userInput + "blahhhhh";
+            console.log(mealPlan);
+        }, 2000);
+    } catch (error) {
+        alert('Something went wrong: ' + error.message);
+    }
+
 });
